c49f63063fccf11575bed1ef96258fb2fbcbb83f
MadViper <mkenc12@freeuni.edu.ge>
2018-05-16 12:44:15 +0300

    Update dbms

--- dbms.py
+++ dbms.py
@@ -1,56 +1,72 @@
 import csv
 import os
+import sys
 
 from neo4j.v1 import GraphDatabase, basic_auth
 
-DEFAULT_USER = "app92124873-nY8mkb"
-DEFAULT_PASSWORD = "b.hrxpJRs9BVRq.ZqM9sSBJTMkU2OfR"
-DEFAULT_DRIVER_URI = "bolt://hobby-ooiobamoecilgbkepmmbabal.dbs.graphenedb.com:24786"
+DEFAULT_CSV_FILE = './data/log-ws.csv'
 
-db_username = os.environ.get("GRAPHENEDB_BOLT_USER", default=DEFAULT_USER)
-db_password = os.environ.get("GRAPHENEDB_BOLT_PASSWORD", default=DEFAULT_PASSWORD)
-driver_uri = os.environ.get("GRAPHENEDB_BOLT_URL", default=DEFAULT_DRIVER_URI)
-
-db_driver = GraphDatabase.driver(driver_uri, auth=basic_auth(db_username, db_password))
+db_driver = GraphDatabase.driver(
+    os.environ.get("BOLT_URL"),
+    auth=basic_auth(os.environ.get("DB_USER"),
+                    os.environ.get("DB_PASSWORD"))
+)
 
 
-def run_query(driver, query, *args, **kwargs):
-    with driver.session() as session:
+def get_csv_file():
+    try:
+        return sys.argv[1]
+    except IndexError:
+        return DEFAULT_CSV_FILE
+
+
+def run_query(query, *args, **kwargs):
+    with db_driver.session() as session:
         return session.run(query, *args, **kwargs)
 
 
-def create_works_with_relations(driver):
+def create_works_with_relations():
     query = '''
-    MATCH ()<-[i1:includes]-(c:case)-[i2:includes]->()
-    WHERE i1.timestamp - i2.timestamp = 1 and i1.performer<>i2.performer
-    WITH i1.performer as p1name, i2.performer as p2name, COUNT(*) as count
+    MATCH (a1)<-[i1:includes]-(c:case)-[i2:includes]->(a2)
+    WHERE i1.timestamp - i2.timestamp = 1
+    WITH
+        c.id as case,
+        i1.performer as p1name,
+        i2.performer as p2name,
+        i1.timestamp as finish,
+        i2.timestamp as start
     MERGE (p1:performer {name: p1name})
     MERGE (p2:performer {name: p2name})
-    MeRGE (p1)-[w:works_with {count: count}]->(p2)
-    return p1, w, p2
+    MERGE (p2)-[w:works_with {case: case, start: start, finish: finish}]->(p1)
+    RETURN p1, w, p2
     '''
-    run_query(driver, query)
+    run_query(query)
 
 
-def seed_data(driver):
-    with open('./data/log-ws.csv') as csv_file:
+def seed_data(records):
+    for record in records:
+        create_query = '''
+        MERGE (c:case {id: {case}})
+        MERGE (a:activity {name: {activity}})
+        MERGE (p:performer {name: {performer}})
+        MERGE (p)-[:performed {case: {case}, timestamp: toFloat({timestamp})} ]->(a)
+        MERGE (c)-[:includes {performer: {performer}, timestamp: toFloat({timestamp})}]->(a)
+        '''
+
+        run_query(create_query, record)
+
+
+def drop_data():
+    run_query('MATCH (n) DETACH DELETE n')
+
+
+def main():
+    drop_data()
+    with open(get_csv_file()) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=";")
-        for row in reader:
-            query = '''
-            MERGE (c:case {id: {case}})
-            MERGE (a:activity {name: {activity}})
-            MERGE (p:performer {name: {performer}})
-            MERGE (p)-[:performed {case: {case}, timestamp: toFloat({timestamp})} ]->(a)
-            MERGE (c)-[:includes {performer: {performer}, timestamp: toFloat({timestamp})}]->(a)
-            '''
-
-            run_query(driver, query, row)
+        seed_data(reader)
+    create_works_with_relations()
 
 
-def drop_data(driver):
-    run_query(driver, 'MATCH (n) DETACH DELETE n')
-
-
-drop_data(db_driver)
-seed_data(db_driver)
-create_works_with_relations(db_driver)
+if __name__ == '__main__':
+    main()
