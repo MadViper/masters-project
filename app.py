13eda63569b97e03bd6b0ae51fc8d9e97707a3be
MadViper <mkenc12@freeuni.edu.ge>
2018-07-11 16:37:55 +0300

    Initialize database on file upload (in the background)

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import json
 import os
+import threading
 
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect
 from flask import render_template, make_response
-from flask import send_from_directory
 from werkzeug.utils import secure_filename
 
+import dbms
 from dal import DAL, QueryBuilder
 from logger import logger
 
@@ -36,7 +37,7 @@ def upload_file():
 
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file part in the request')
             return redirect(request.url)
 
         file = request.files['file']
@@ -44,20 +45,24 @@ def upload_file():
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            flash('File not selected')
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+        if allowed_file(file.filename):
+            file_name = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            file.save(file_path)
+            flash("File uploaded successfully")
+            # Start worker to initialize database
+            threading.Thread(
+                name="neo4j-init",
+                target=dbms.load_file,
+                args=(file_path,)
+            ).start()
+        else:
+            flash('File should have .csv extension')
 
     return render_template("public/uploader.html")
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 @app.route('/explorer')
